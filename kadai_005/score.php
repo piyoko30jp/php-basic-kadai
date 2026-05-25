5aad2851e281ceb9ddddacf79114a55860a76b70
piyoko30jp <lovesunflowerpiyoko@gmail.com>
2026-05-25 21:21:59 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -8,17 +8,23 @@
 
 <body>
 <?php
-// 10個の整数を配列に準備する
-$numbers = [80, 60, 55, 40, 100, 25, 80, 95, 30, 60];
+// 10個の変数に整数を代入する
+$score1 =80;
+$score2 =60;
+$score3 =55;
+$score4 =40;
+$score5 =100;
+$score6 =25;
+$score7 =80;
+$score8 =95;
+$score9 =30;
+$score10 =60;
 
-// 配列の合計値を計算する
-$total = array_sum($numbers);
-
-// 配列の要素数（個数）を数える（今回は 10）
-$count = count($numbers);
+// 合計値を計算する
+$total = $score1+$score2+$score3+$score4+$score5+$score6+$score7+$score8+$score9+$score10;
 
 // 合計を個数で割って平均値を出す
-$average = $total / $count;
+$average = $total / 10;
 
 // 結果を画面に表示する
 echo $average;
